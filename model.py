7f774bce4be4f0a9eae92e05c282072bb1809220
jiaxiaogang14 <897478860@qq.com>
2020-06-14 02:04:13 +0800

    wts

---
+++ model.py
@@ -0,0 +1,170 @@
+#--Xiaogang Jia <jiaxiaogang@nudt.edu.cn>
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.ops import gen_nn_ops
+import corr1d
+
+nfCV = 96 * 3
+
+bneps = 1e-8
+ifbn = 1    
+
+encs = [nfCV, 144, 96]
+disp = [3, 32, 48, 64, 80, 96]
+
+
+# Remove BNs (use after replacing filters with popstats)
+def toTest():
+    global ifbn
+    ifbn = 0
+
+def conv(net, name, inp, ksz, bn=0, st=1, relu=True, rt=1):  
+    if len(ksz) > 2:
+        inch = ksz[2]  
+    else:
+        inch = int(inp.get_shape()[-1])  
+
+    ksz = [ksz[0], ksz[0], inch, ksz[1]]  
+
+    wnm = name + "_w"  
+    if wnm in net.weights.keys():
+        w = net.weights[wnm]
+    else:
+        sq = np.sqrt(3.0 / np.float32(ksz[0] * ksz[1] * ksz[2]))
+        w = tf.Variable(tf.random_uniform(ksz, minval=-sq, maxval=sq, dtype=tf.float32))
+        net.weights[wnm] = w
+        net.wd = net.wd + tf.nn.l2_loss(w)
+
+    if rt != 1:
+        out = tf.nn.convolution(inp, w, 'SAME', [st, st], rt) 
+    else:
+        out = tf.nn.conv2d(inp, w, [1, st, st, 1], 'SAME')
+
+    if bn == 1:
+        mu, vr = tf.nn.moments(out, [0, 1, 2]) 
+        net.bnvals[name + '_m'] = mu
+        net.bnvals[name + '_v'] = vr
+        out = tf.nn.batch_normalization(out, mu, vr, None, None, bneps)
+
+    bnm = name + "_b"
+    if bnm in net.weights.keys():
+        b = net.weights[bnm]
+    else:
+        b = tf.Variable(tf.constant(0, shape=[ksz[-1]], dtype=tf.float32))
+        net.weights[bnm] = b
+    out = out + 2.0 * b
+
+    if relu:
+        out = tf.nn.relu(out)
+
+    return out
+
+
+def resconv(net, pfx, inp, out_channel, in_channel, bn, rt=1):
+    out = inp
+
+    out1 = conv(net, pfx + '_1', out, [3, out_channel, in_channel], bn, 1, True, rt)
+    out1 = conv(net, pfx + '_2', out1, [3, out_channel, out_channel], bn, 1, False, rt)
+    out = out + out1
+    out = tf.nn.relu(out)
+
+    return out
+
+
+def downconv(net, pfx, inp, out_channel, in_channel, bn):
+    out = inp
+
+    out1 = conv(net, pfx + '_l', out, [1, out_channel, in_channel], bn, 2, False)
+
+    out2 = conv(net, pfx + '_r1', out, [3, out_channel, in_channel], bn, 2, True)
+    out2 = conv(net, pfx + '_r2', out2, [3, out_channel, out_channel], bn, 1, False)
+
+    out = out1 + out2
+    out = tf.nn.relu(out)
+
+    return out
+
+
+def fpn(net, pfx, left, right, h_channel, l_channel, bn):
+    shap = tf.shape(left)
+    left = conv(net, pfx + '_1', left, [1, l_channel, h_channel], bn, 1, True)
+    right = conv(net, pfx + '_2', right, [1, l_channel, l_channel], bn, 1, True)
+
+    right = tf.image.resize_bilinear(right, tf.stack([shap[1], shap[2]]))
+
+    out = left + right
+    out = conv(net, pfx + '_3', out, [3, l_channel, l_channel], bn, 1, True)
+
+    return out
+
+
+def ffm(net, pfx, left, right, h_channel, l_channel, bn):
+    shap = tf.shape(left)
+    right = tf.image.resize_bilinear(right, tf.stack([shap[1], shap[2]]))
+    out = tf.concat([left, right], axis=3)
+    out = conv(net, pfx + '_1', out, [3, h_channel, h_channel + l_channel], 0, 1, True)
+
+    out1 = conv(net, pfx + '_2', out, [1, h_channel, h_channel], bn, 1, True)
+
+    out1 = tf.reduce_mean(out1, [1, 2], True)
+    out1 = conv(net, pfx + '_3', out1, [1, h_channel, h_channel], 0, 1, True) 
+    out1 = conv(net, pfx + '_4', out1, [1, h_channel, h_channel], 0, 1, False)
+    out1 = tf.nn.sigmoid(out1)
+
+    out2 = out * out1
+    out = out + out2
+
+    return out
+
+
+class Net:
+    def __init__(self):
+        self.weights = {}
+        self.bnvals = {}
+        self.wd = 0.
+
+    # Encode images to feature tensor
+    def predict(self, img, cv, lrl, rmg):
+        fts = cv  
+        for i in range(len(encs) - 1):  
+            fts = conv(self, 'enc%d' % i, fts, [1, encs[i + 1], encs[i]], ifbn, 1, True)
+        fts = resconv(self, 'res2', fts, encs[-1], encs[-1], 0)
+        fts = resconv(self, 'res2_2', fts, encs[-1], encs[-1], 0)
+      
+        disl = conv(self, 'con1_l', img, [7, disp[1], disp[0]], ifbn, 2)  
+        disr = conv(self, 'con1_r', rmg, [7, disp[1], disp[0]], ifbn, 2)
+        disl = conv(self, 'con2_l', disl, [5, disp[2], disp[1]], ifbn, 2)  
+        disr = conv(self, 'con2_r', disr, [5, disp[2], disp[1]], ifbn, 2)
+
+        corr = corr1d.correlation1d(disl, disr, 1, 48, 1, 1,
+                                    48)
+
+        corr = resconv(self, 'res6', corr, disp[2], disp[2], 0)
+        corr = resconv(self, 'res6_2', corr, disp[2], disp[2], 0)  
+
+        corr2 = downconv(self, 'res7', corr, disp[3], disp[2], 0)
+        corr2 = resconv(self, 'res7_2', corr2, disp[3], disp[3], 0)  
+
+        corr3 = downconv(self, 'res8', corr2, disp[4], disp[3], 0)
+        corr3 = resconv(self, 'res8_2', corr3, disp[4], disp[4], 0)  
+
+        corr4 = downconv(self, 'res9', corr3, disp[5], disp[4], 0)
+        corr4 = resconv(self, 'res9_2', corr4, disp[5], disp[5], 0)  
+
+        corr3_4 = fpn(self, 'fp1', corr3, corr4, disp[4], disp[5], 0)
+        corr2_3 = fpn(self, 'fp2', corr2, corr3_4, disp[3], disp[5], 0)
+        corr1_2 = fpn(self, 'fp3', corr, corr2_3, disp[2], disp[5], 0)
+        corr0_1 = ffm(self, 'ffm', fts, corr1_2, disp[5], disp[5], ifbn)
+
+        out = corr0_1
+
+        out = tf.nn.relu(conv(self, 'out', out, [1, 1, 96], 0, 1, False) + 128.0)
+
+        shp = tf.shape(img)
+
+        out = tf.image.resize_bilinear(out, tf.stack([shp[1], shp[2]]))
+
+        return tf.squeeze(out, axis=-1)
+
+
+
---
+++ pdata.py
@@ -0,0 +1,136 @@
+#--Xiaogang Jia <jiaxiaogang@nudt.edu.cn>
+import tensorflow as tf
+import numpy as np
+import slib
+import re
+
+def lowrescv(limg,rimg,imsz,dmax=96):
+        
+        limg = tf.nn.avg_pool(limg,[1,2,2,1],[1,2,2,1],'VALID')
+        rimg = tf.nn.avg_pool(rimg,[1,2,2,1],[1,2,2,1],'VALID')
+        lim0 = limg
+        
+        limg, rimg = tf.image.rgb_to_yuv(limg), tf.image.rgb_to_yuv(rimg)  
+        limg, rimg = slib.census(limg), slib.census(rimg)
+         
+        cv = slib.hamming(limg,rimg,0,0,imsz[0]//2,imsz[1]//2,dmax) 
+        cv = (cv-[11.08282948,0.02175535,0.02679042])*[0.1949711,35.91432953,26.79782867]
+
+        shp = tf.shape(cv)
+        cv = tf.reshape(cv,tf.stack([shp[0],shp[1],shp[2],-1]))
+        return cv,lim0
+
+def readPFM(file):
+    file = open(file, 'rb')
+
+    header = file.readline().rstrip()
+    if header.decode("ascii") == 'PF':
+        color = True
+    elif header.decode("ascii") == 'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+
+    dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode("ascii"))
+    if dim_match:
+        width, height = list(map(int, dim_match.groups()))
+    else:
+        raise Exception('Malformed PFM header.')
+
+    scale = float(file.readline().decode("ascii").rstrip())
+    if scale < 0: 
+        endian = '<
+        scale = -scale
+    else:
+        endian = '>' 
+
+    data = np.fromfile(file, endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+
+    data = np.reshape(data, shape)
+    data = np.flipud(data)
+    return data, scale
+
+class dataset:
+    def graph(self):
+        self.left = []
+        self.right = []
+        self.gtl = []
+        self.gtr = []
+
+        # Create placeholders for image names
+        for i in range(self.bsz):
+            self.left.append(tf.placeholder(tf.string))
+            self.right.append(tf.placeholder(tf.string))
+            self.gtl.append(tf.placeholder(tf.string))
+            self.gtr.append(tf.placeholder(tf.string))
+
+        # Load Images
+        limgs, rimgs, gts = [], [], []
+        imsz = self.imsz
+
+        for i in range(self.bsz):
+            limg = tf.image.decode_jpeg(tf.read_file(self.left[i]),channels=3)
+            rimg = tf.image.decode_jpeg(tf.read_file(self.right[i]),channels=3)
+            
+            gtl = tf.py_func(lambda x: readPFM(x)[0],[self.gtl[i]],tf.float32)
+            gtl = tf.expand_dims(gtl, axis=2)
+            gtl.set_shape([None,None,1])
+            gtr = tf.py_func(lambda x: readPFM(x)[0],[self.gtr[i]],tf.float32)
+            gtr = tf.expand_dims(gtr, axis=2)
+            gtr.set_shape([None,None,1])
+            
+
+            limgs.append(limg)
+            limgs.append(rimg[:,::-1,:])  
+            rimgs.append(rimg)
+            rimgs.append(limg[:,::-1,:])
+            gts.append(gtl)
+            gts.append(gtr[:,::-1,:])
+
+        # Stack & Convert
+        limgs = tf.to_float(tf.stack(limgs))/255.0
+        rimgs = tf.to_float(tf.stack(rimgs))/255.0
+        gts = tf.to_float(tf.stack(gts))
+        mask = tf.to_float(gts < np.float32(255.0))
+
+        self.cv, lrl = lowrescv(limgs,rimgs,self.imsz)   
+        self.lrl = 2.0*lrl-1.0  
+
+        self.limgs = 2.0*limgs-1.0
+        self.rimgs = 2.0*rimgs-1.0  
+        self.disp = gts
+        self.mask = mask
+
+    def fdict(self,ids):
+        fd = {}
+        assert len(ids) == self.bsz
+        for i in range(self.bsz):
+            fd[self.left[i]] = ids[i] % self.ligrp
+            fd[self.right[i]] = ids[i] % self.rigrp
+            fd[self.gtl[i]] = ids[i] % self.lggrp
+            fd[self.gtr[i]] = ids[i] % self.rggrp
+            
+        return fd
+
+    '''
+    bsz = Batch Size
+    csz = Crop Size (of final depth map output)
+    rsz = Receptive Field of NN (to pad image)
+    nsz = Filter Size for Smoothing (to pad cost volume)
+
+    Run fetchOp to get lrgb, disp, mask, cv
+    '''
+    def __init__(self,bsz, \
+                 ligrp=('frames_cleanpass','left','png'), \
+                 rigrp=('frames_cleanpass','right','png'), \
+                 lggrp=('disparity','left','pfm'), \
+                 rggrp=('disparity','right','pfm'), \
+                 imsz=[540,960]):
+
+        self.bsz = bsz
+        self.ligrp,self.rigrp = ligrp, rigrp
+        self.lggrp,self.rggrp = lggrp, rggrp
+        self.imsz = imsz
+
+        self.graph()
